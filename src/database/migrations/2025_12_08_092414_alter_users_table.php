7f06737fd3f26e88abdae754de3e7ba2c8b1a0b1
kouheigit <ookawakouhei777@gmail.com>
2025-12-08 18:55:29 +0900

    feat:userテーブルの追加実装

--- src/database/migrations/2025_12_08_092414_alter_users_table.php
+++ src/database/migrations/2025_12_08_092414_alter_users_table.php
@@ -8,6 +8,20 @@ class AlterUsersTable extends Migration
 {
     public function up(){
         Schema::table('users', function (Blueprint $table) {
+            // 会員ID
+            $table->string('member_id')->unique()->nullable->after('id');
+
+            $table->string('last_name')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_kana')->nullable();
+            $table->string('zip1',3)->nullable();
+            $table->string('zip2',4)->nullable();
+            $table->string('address1')->nullable();
+            $table->string('address2')->nullable();
+            $table->string('tel',20)->nullable();
+
+            // 会社情報
+
 
         });
     }
@@ -18,6 +32,3 @@ class AlterUsersTable extends Migration
     }
 }
 
-
-
- 
