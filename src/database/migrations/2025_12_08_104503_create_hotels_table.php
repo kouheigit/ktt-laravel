72b28a7f6adefab7529359c4d470497611082d49
kouheigit <ookawakouhei777@gmail.com>
2025-12-08 19:57:54 +0900

    feat:hotelテーブルの追加実装

--- src/database/migrations/2025_12_08_104503_create_hotels_table.php
+++ src/database/migrations/2025_12_08_104503_create_hotels_table.php
@@ -4,12 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateHotelsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
@@ -17,11 +14,11 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('hotels');
     }
-};
+}
+
+
+
