fa3af535d64a77f7b0d6a5b560cae35a41e91651
kouheigit <ookawakouhei777@gmail.com>
2025-12-08 20:59:45 +0900

    feat:calendarsテーブルの追加実装

--- src/database/migrations/2025_12_08_113502_create_calendars_table.php
+++ src/database/migrations/2025_12_08_113502_create_calendars_table.php
@@ -12,7 +12,17 @@ class CreateCalendarsTable extends Migration
     {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
+            $table->foreinId('user_id')->constrained();
+            $table->date('date')->comment('対象日');
+            $table->date('date')->nullable()->comment('期間開始日');
+            $table->date('end_date')->nullable()->comment('期間終了');
+            $table->integer('status')->default(1)->comment('1:予約可,2予約中,3予約済,9:休業');
             $table->timestamps();
+
+            //インデックス
+            $table->index(['hotel_id','date']);
+            $table->index(['user_id','start_date']);
+
         });
     }
 
@@ -24,7 +34,6 @@ class CreateCalendarsTable extends Migration
         Schema::dropIfExists('calendars');
     }
 };
-
 
 
 
