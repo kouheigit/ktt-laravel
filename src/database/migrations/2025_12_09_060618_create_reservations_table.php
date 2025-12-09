ef49a82b7d692202da590ceaf007a71882dfea9c
kouheigit <ookawakouhei777@gmail.com>
2025-12-09 15:21:05 +0900

    feat:reservationsテーブルに宿泊情報を追加

--- src/database/migrations/2025_12_09_060618_create_reservations_table.php
+++ src/database/migrations/2025_12_09_060618_create_reservations_table.php
@@ -18,7 +18,16 @@ class CreateReservationsTable extends Migration
             $table->foreignId('user_id')->constrained()->comment('予約者');
             $table->foreignId('owner_id')->nullable()->constrained();
             $table->foreignId('invitation_id')->nullable()->constrained();
-            
+
+
+            // 宿泊情報
+            $table->date('checkin_date')->comment('チェックイン日');
+            $table->date('checkout_date')->comment('チェックアウト日');
+            $table->time('checkin_time')->nullable()->comment('チェックイン時刻');
+            $table->time('checkout_time')->nullable()->comment('チェックアウト時刻');
+            $table->integer('day')->default(1)->comment('宿泊日数');
+
+
             $table->timestamps();
         });
     }
