f01906575b01e7858a0ebc7df2cbdcfe9d17af7c
kouheigit <ookawakouhei777@gmail.com>
2025-12-09 17:27:24 +0900

    userモデルの実装(完成)

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -32,10 +32,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
@@ -44,10 +41,7 @@ class User extends Authenticatable
      */
     protected function casts(): array
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return ['email_verified_at' => 'datetime'];
     }
 }
 
