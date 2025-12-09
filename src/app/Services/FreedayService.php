241a58ba8ba3d7928ac0641592a61c83c5ab43e7
kouheigit <ookawakouhei777@gmail.com>
2025-12-09 20:06:17 +0900

    feat:getFreedaysの実装

---
+++ src/app/Services/FreedayService.php
@@ -0,0 +1,13 @@
+<?php
+// app/Services/FreedayService.php
+
+namespace App\Services;
+use App\Models\Freeday;
+use App\Models\User;
+use Carbon\Carbon;
+
+class FreedayService{
+    public function getFreedays(User $user){
+        
+    }
+}
