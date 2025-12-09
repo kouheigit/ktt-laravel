566316acee72a152288c7d724517e5e4b6a74ca7
kouheigit <ookawakouhei777@gmail.com>
2025-12-09 20:15:52 +0900

    feat:FreedaysService@getFreedaysの実装

--- src/app/Services/FreedayService.php
+++ src/app/Services/FreedayService.php
@@ -8,6 +8,14 @@ use Carbon\Carbon;
 
 class FreedayService{
     public function getFreedays(User $user){
+        $now = Carbon::now();
+
+        return Freeday::where('user_id',$user->id)
+            ->where('end_date','>=',$now->format('Y-m-d'))
+            ->where('freedays','>',0)
+            ->where('status',1)
+            ->orderBy('end_date','asc')
+            ->get();
         
     }
 }
