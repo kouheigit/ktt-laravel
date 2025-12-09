aab20f999af3043f10f6baf7a03ee077aa80328a
kouheigit <ookawakouhei777@gmail.com>
2025-12-09 14:07:32 +0900

    freedayテーブルの実装

--- src/database/migrations/2025_12_09_044817_create_freedays_table.php
+++ src/database/migrations/2025_12_09_044817_create_freedays_table.php
@@ -13,15 +13,23 @@ return new class extends Migration
     {
         Schema::create('freedays', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('freedays')->default(0)->comment('残り泊数');
+            $table->date('start_date')->comment('利用開始日');
+            $table->date('end_date')->comment('有効期限');
+            $table->integer('status')->default(1);
             $table->timestamps();
+
+            $table->index(['user_id','end_date']);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('freedays');
     }
 };
+
