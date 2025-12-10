138e7cf71ee8959891e10c5ae9fab102baa8fa2e
kouheigit <ookawakouhei777@gmail.com>
2025-12-10 17:17:26 +0900

    feat:ReservationController@indexの実装

--- src/app/Http/Controllers/ReservationController.php
+++ src/app/Http/Controllers/ReservationController.php
@@ -23,6 +23,34 @@ class ReservationController extends Controller
     {
         $this->freeday_service = $freeday_service;
     }
-    
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+
+        //2年分のFIXDAYを取得
+        $start_date = Carbon::now()->firstOfYear();
+        $end_date = $start_date->copy()->addYear(2)->endOfyear();
+
+        $calendars = Calendar::where('user_id',$user->id)
+            ->whereBetween('start_date',[$start_date,$end_date])
+            ->orderBy('start_date',[$start_date,$end_date])
+            ->get();
+
+        //FREEDAYSを取得
+        $freedays = $this->freeday_service->getFreedays($user);
+
+        // 予約を取得
+        $reservations = Reservation::where('owner_id',$user->id)
+            ->whereIn('status',[
+                ReservationConst::STATUS_APPLYING,
+                ReservationConst::STATUS_UNDER_RESERVATION,
+                ReservationConst::STATUS_RESERVED
+            ])
+            ->orderBy('checkin_date','asc')
+            ->get();
+        return view('reservation.index',compact('calendars','freedays','reservations'));
+    }
+  
+
 
 }
