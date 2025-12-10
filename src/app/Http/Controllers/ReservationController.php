40c13cd7ab37cda35f28c676fde6e007f5d8c839
kouheigit <ookawakouhei777@gmail.com>
2025-12-10 17:04:24 +0900

    feat:Reservation@constructの実装

--- src/app/Http/Controllers/ReservationController.php
+++ src/app/Http/Controllers/ReservationController.php
@@ -1,4 +1,5 @@
 <?php
+// app/Http/Controllers/ReservationController.php
 
 namespace App\Http\Controllers;
 use App\Models\Reservation;
@@ -18,65 +19,10 @@ class ReservationController extends Controller
 {
     private $freeday_service;
 
-    public function__construct(FreedayService $freeday_service)
+    public function __construct(FreedayService $freeday_service)
     {
         $this->freeday_service = $freeday_service;
     }
+    
 
-    public function index(Request $request)
-    {
-        $user = Auth::user();
-
-        $start_date = Carbon::now()->firstOfYear();
-        $end_date = $start_date->copy()->addYears(2)->endOfYear();
-
-        $calendars = Calendar::where('user_id',$user->id)->whereBetween('start_date',[$start_date,$end_date])
-            ->orderBy('start_date','asc')
-            ->get();
-
-        // FREEDAYSを取得
-        $freedays = $this->freeday_service->getFreedays($user);
-
-        //予約を取得
-        $reservations = Reservation::where('owner_id',$user->id)
-            ->whereIn('status',[
-                ReservationConst::STATUS_APPLYING,
-                ReservationConst::STATUS_UNDER_RESERVATION,
-                ReservationConst::STATUS_RESERVED,
-                ])
-            ->orderBy('checkin_date','asc')
-            ->get();
-        return view('reservation.index',compact('calendars','freedays','reservations'));
-    }
-
-    public function create(Request $request)
-    {
-        $calendar_id = $request->calendar_id;
-
-        $fr = $request->fr;//フリーデイID
-
-        //FIXDAYが予約失敗した時の処理
-        if($calendar_id){
-            //FIXDAY予約
-            $calendar_id = Calendar::findOrfail($calendar_id);
-
-            return view('reservation.create',compact('calendar'));
-        }
-
-        //FREEDAYの予約失敗した時の処理
-        if($fr){
-            //FREEDAY予約
-            $freeday = Freeday::findOrFail($fr);
-
-            return view('reservation.create_freeday',compact('freeday'));
-        }
-        abort(404);
-    }
-
-    // サービス選択画面
-    public function service(Request $request)
-   {
-
-   }
 }
-
