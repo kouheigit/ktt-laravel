16647ec3e79e8471612825ee6dbace523092355d
kouheigit <ookawakouhei777@gmail.com>
2025-12-11 14:13:19 +0900

    ”Add:CalendarController@indexの追加”

--- src/app/Http/Controllers/CalendarController.php
+++ src/app/Http/Controllers/CalendarController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Calendar;
+use App\Models\Reservation;
+use App\Models\Holiday;
+use App\Consts\ReservationConst;
 use Illuminate\Http\Request;
+use Auth;
+
+use Carbon\Carbon;
 
 class CalendarController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        
+    }
 }
