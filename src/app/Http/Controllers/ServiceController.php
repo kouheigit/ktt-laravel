a72425457be65cff07c681066cb97d4a0d7ac976
kouheigit <ookawakouhei777@gmail.com>
2025-12-13 06:17:26 +0900

    ”Add:ServiceController@indexの追加”

--- src/app/Http/Controllers/ServiceController.php
+++ src/app/Http/Controllers/ServiceController.php
@@ -4,7 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+namespace App\Http\Controllers;
+use App\Models\Service;
+use App\Models\ServiceOption;
+use App\Models\Cart;
+use App\Models\CartDetail;
+use App\Models\Reservation;
+use Auth;
+
+
 class ServiceController extends Controller
 {
-    //
+    public function index()
+    {
+        
+    }
 }
