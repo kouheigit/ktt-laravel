999fb5b1fb13f46af5468964a8d6f8f3ba76fd89
kouheigit <ookawakouhei777@gmail.com>
2025-12-14 14:55:51 +0900

    chore: CartControllerにModel・Facadeのuse文を追加

--- src/app/Http/Controllers/CartController.php
+++ src/app/Http/Controllers/CartController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+namespace App\Http\Controllers;
+use App\Models\Cart;
+use App\Models\CartDetail;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\Reservation;
+use Auth;
+
+use DB;
+
 class CartController extends Controller
 {
-    //
+    
 }
