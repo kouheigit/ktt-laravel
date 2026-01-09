f3d48e112bc63b333067cb87fb0db3d3f69e2867
kouheigit <ookawakouhei777@gmail.com>
2026-01-09 14:15:05 +0900

    Add PointServeceへのaddPointの新規追加

---
+++ src/app/Services/PointService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Services;
+use App\Models\UserPoint;
+use App\Models\UserPointLog;
+use App\Models\User;
+use Carbon\Carbon;
+use DB;
+
+class PointService
+{
+    /**
+     * ポイント付与
+     *
+     * @param int $userId ユーザーID
+     * @param int $point ポイント数
+     * @param string $reason 理由
+     * @param string $from 有効開始日
+     * @param string $to 有効期限
+     */
+    public function addPoint($userId,$point,$reason,$from,$to)
+    {
+    }
+}
