6652462cc2d3f0912932c6664a7532d4aece578a
kouheigit <ookawakouhei777@gmail.com>
2026-01-11 16:30:16 +0900

    Add:MypageController@indexメソットの追加

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use App\Models\Freeday;
+use App\Models\UserPoint;
+use App\Services\FreedayService;
+use App\Services\PointService;
+use App\Consts\ReservationConst;
+use App\Consts\UserConst;
+use Auth;
+use Carbon\Carbon;
 
 class MypageController extends Controller
 {
-    //
+    public function index(){
+
+    }
 }
