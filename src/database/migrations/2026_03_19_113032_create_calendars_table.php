977aa81cb1601248ebc30c264dce8840813397bc
kouheigit <ookawakouhei777@gmail.com>
2026-03-19 21:10:58 +0900

    feat:create_calendarsテーブルの実装中

--- src/database/migrations/2026_03_19_113032_create_calendars_table.php
+++ src/database/migrations/2026_03_19_113032_create_calendars_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('calendars', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('hotel_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->date('date')->comment('対象日');
+            $table->date('start_date')->nullable()->comment('期間開始日');
+            $table->date('end_date')->nullable()->comment('期間終了日');
+            $table->integer('status')->default(1)->comment('1:予約可,2:予約中,3予約済,9:休業');
+
+            //検索用のindex(hotel_idがselectでホテルを選ぶ、dateは予約日
+            $table->index(['hotel_id','date']);
+            //履歴表示のindex
+            $table->index(['user_id','start_date']);
+
             $table->timestamps();
         });
     }
@@ -25,3 +37,6 @@ return new class extends Migration
         Schema::dropIfExists('calendars');
     }
 };
+
+
+
