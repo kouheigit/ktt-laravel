74d89a9ac7ea6641bade774778667eb95b8ff60a
kouheigit <ookawakouhei777@gmail.com>
2026-03-19 20:28:05 +0900

    feat:hotel usersテーブルの実装

--- src/database/migrations/2026_03_19_111041_create_hotel_user_table.php
+++ src/database/migrations/2026_03_19_111041_create_hotel_user_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('hotel_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('hotel_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
+
+            // 複合ユニークキー
+            $table->unique(['hotel_id','user_id']);
         });
     }
 
@@ -25,3 +30,4 @@ return new class extends Migration
         Schema::dropIfExists('hotel_user');
     }
 };
+
