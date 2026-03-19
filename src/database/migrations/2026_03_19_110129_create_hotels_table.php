5364cfa42f241a208bc27cc5e47cf4629e95bf45
kouheigit <ookawakouhei777@gmail.com>
2026-03-19 20:07:48 +0900

    feat:hotelsテーブルの実装完成

--- src/database/migrations/2026_03_19_110129_create_hotels_table.php
+++ src/database/migrations/2026_03_19_110129_create_hotels_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('hotels', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->comment('ホテル名');
+            $table->string('address')->nullable()->comment('住所');
+            $table->text('description')->nullable()->comment('説明');
+            $table->integer('status')->default(1)->comment('1:有効,0:無効');
             $table->timestamps();
         });
     }
