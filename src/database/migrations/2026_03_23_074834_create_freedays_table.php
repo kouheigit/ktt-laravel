ce2d833d5ffe9e6898eb0306b3396d5833e485bf
kouheigit <ookawakouhei777@gmail.com>
2026-03-23 17:03:16 +0900

    feat:feat:calendar_optionテーブルのカラムの追加

---
+++ src/database/migrations/2026_03_23_074834_create_freedays_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('freedays', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->integer('freedays')->default(0)->comment('残り泊数');
+            $table->date('star_date')->comment('利用開始日');
+            $table->date('end_date')->comment('有効期限');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+            $table->index(['user_id','end_date']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('freedays');
+    }
+};
