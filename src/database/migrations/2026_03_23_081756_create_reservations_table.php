622cf39323893ab5f017642028a2ed0197fb4f2c
kouheigit <ookawakouhei777@gmail.com>
2026-03-23 17:28:29 +0900

    feat:holidayテーブル外部キーの実装

---
+++ src/database/migrations/2026_03_23_081756_create_reservations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hotel_id')->constrained();
+            $table->foreignId('user_id')->constrained()->comment('予約者');
+            $table->foreignId('owner_id')->nullable()->constrained('users')->comment('オーナー');
+            $table->foreignId('calendar_id')->nullable()->constrained();
+            $table->foreignId('invitation_id')->nullable()->constrained();
+
+      
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
