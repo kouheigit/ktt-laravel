f4a4c7ed62e4903ff8e7271da25f0ef330d2c79d
kouheigit <ookawakouhei777@gmail.com>
2026-03-27 19:09:28 +0900

    feat:consumeFreedayの実装

--- src/app/Services/FreedayService.php
+++ src/app/Services/FreedayService.php
@@ -2,11 +2,8 @@
 
 
 namespace App\Services;
-
 use App\Models\Freeday;
-
 use App\Models\User;
-
 use Carbon\Carbon;
 
 class FreedayService
@@ -14,22 +11,56 @@ class FreedayService
     public function getFreedays(User $user)
     {
         $now = Carbon::now();
-        return Freeday::where('user_id',$user->id)
-            ->where('end_date','>=',$now->format('Y-m-d'))
-            ->where('status',1)
-            ->orderBy('end_date','asc')
+        return Freeday::where('user_id', $user->id)
+            ->where('end_date', '>=', $now->format('Y-m-d'))
+            ->where('status', 1)
+            ->orderBy('end_date', 'asc')
             ->get();
     }
+
     //今年度の最大フリーデイ泊数を取得
     public function getYearMaxFreedaysNum(User $user)
     {
         $standOfYear = Carbon::now()->startOfYear();
         $endOfYear = Carbon::now()->endOfYear();
 
-        return Freeday::where('user_id',$user->id)
-            ->whereBetween('start_date',[$standOfYear, $endOfYear])
+        return Freeday::where('user_id', $user->id)
+            ->whereBetween('start_date', [$standOfYear, $endOfYear])
             ->sum('freedays');
     }
+
+    //フリーデイ利用可能チェック
+    public function canUseFreeday(Freeday $freeday, $days)
+    {
+        //残り泊数
+        if ($freeday->freedays < $days) {
+            return false;
+        }
+
+        //有効期限チェック
+        if (Carbon::parse($freeday->endday)) {
+            return false;
+        }
+        //利用開始日チェック
+        $availableFrom = Carbon::parse($freeday->start_date)->fistOfMonth()->sunMonths(18);
+        if (Carbon::now()->iswBefore($availableFrom)) {
+            return false;
+        }
+        return true;
+    }
+    public function consumeFreeday(Freeday $freeday,$days)
+    {
+        if(!$this->canUseFreeday($freeday,$days)){
+            throw new \Exception('フリーデイが利用できません');
+        }
+        $freeday->decrement('freedays', $days);
+
+        return $freeday;
+    }
 }
+
+
+
+
 
 
