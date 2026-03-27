b84deff2bfc3cd6daa1a8f6cb5cbe34de4ee755e
kouheigit <ookawakouhei777@gmail.com>
2026-03-27 21:32:05 +0900

    feat:CalenderController@indexの実装

--- src/app/Http/Controllers/CalendarOptionController.php
+++ src/app/Http/Controllers/CalendarOptionController.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Calendar;
+use App\Models\Reservation;
+use App\Models\Holiday;
+use App\Consts\ReservationConst;
+use Auth;
+use Carbon\Carbon;
+
 
 class CalendarOptionController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $user = Auth::user();
+
+        //年月指定（デフォルトは今月）
+        $year = $request->input('year',now()->year);
+        $month = $request->input('month',now()->month);
+
+        $date = Carbon::createFromDate($year,$month,1);
+        //該当つきのカレンダー取得
+        $calendars = Calendar::where('user_id',$user->id)
+            ->whereYear('start_date',$year)
+            ->whereMonth('start_date',$month)
+            ->with(['hotel'])
+            ->orderBy('start_date','asc')
+            ->get();
+
+    }
 }
+
+
