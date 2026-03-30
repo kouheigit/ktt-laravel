a52a46fc69f13a7071db1a0422961b52a36c18ee
kouheigit <ookawakouhei777@gmail.com>
2026-03-30 17:32:47 +0900

    feat:indexの実装

--- src/app/Http/Controllers/CartController.php
+++ src/app/Http/Controllers/CartController.php
@@ -1,10 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Models\CartDetail;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\Reservation;
+use Auth;
+use DB;
 
 class CartController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Auth::user();
+
+        $cart = Cart::with([
+            'cartDetails.service',
+            'cartDetails.serviceOption'
+        ])->where('user_id',$user->id)->first();
+
+        if(!$cart){
+            return view('cart.index',['cart'=>null,'total_price'=>0]);
+        }
+
+        $total_price = $cart->cartDetails->sum('total_price');
+        //最新予約
+        $last_reservation = Resetvation::getLastReservation();
+
+        return view('cart.index', compact('cart', 'total_price', 'last_reservation'));
+    }
 }
+
