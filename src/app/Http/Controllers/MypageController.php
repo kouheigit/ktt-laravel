3a5e78b0293109fd10036c0dd812cb97726dfb4e
kouheigit <ookawakouhei777@gmail.com>
2026-04-10 18:07:28 +0900

    feat:indexの実装

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -24,7 +24,35 @@ class MypageController extends Controller
         $this->point_service = $point_service;
     }
     public function index(){
-        
-    }
+        $user = Auth::user();
 
+        //利用可能ポイント合計
+        $user_point = $this->point_service->getAvailablePoints($user->id);
+        // 有効期限別ポイント残高
+        $pointbalance = $this->point_service->getPointBalanceByExpiry($user->id);
+
+        // FREEDAY取得（オーナーのみ）
+        $freedays = collect();
+        if((int)$user->type === UserConst::TYPE_OWNER){
+            $freedays = $this->freeday_service->getFreedays($user);
+        }
+        //今後の予約取得
+        $reservations = Reservation::where('user_id',$user->id)
+            ->whereIn('statis',[
+                ReservationConst::STATUS_APPLYING,
+                ReservationConst::STATUS_UNDER_RESERVATION,
+                ReservationConst::STATUS_RESERVED,
+            ])
+            ->where('checkin_date','>=',Carbon::now()->format('Y-m-d'))
+            ->with('hotel')
+            ->orderBy('checkin_date','asc')
+            ->get();
+        return view('mypage.index',compact(
+            'user_point',
+            'pointbalance',
+            'freedays',
+            'reservations',
+            ));
+
+    }
 }
