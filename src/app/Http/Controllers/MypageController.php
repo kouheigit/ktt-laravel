29e7b0c70b008719f4832daa32c21e4ad3bfe252
kouheigit <ookawakouhei777@gmail.com>
2026-04-10 17:44:15 +0900

    feat:constructの実装

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -4,7 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Reservation;
+use App\Services\FreedayService;
+use App\Services\PointService;
+use App\Consts\ReservationConst;
+use App\Consts\UserConst;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
+
 class MypageController extends Controller
 {
-    //
+    private FreedayService $freeday_service;
+    private PointService $point_service;
+
+    public function __construct(
+        FreedayService $freeday_service,
+        PointService $point_service
+    ){
+        $this->freeday_service = $freeday_service;
+        $this->point_service = $point_service;
+    }
 }
